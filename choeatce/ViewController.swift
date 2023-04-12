14bb1ae4ec390b88f2236f920813d0628b93199f
SANGDOLEE <xkdxh625@naver.com>
2023-04-12 13:28:24 +0900

    Initial Commit

---
+++ choeatce/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  choeatce
+//
+//  Created by 이상도 on 2023/04/10.
+//
+
+import UIKit
+import NMapsMap
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .orange
+      
+    }
+
+
+}
+
