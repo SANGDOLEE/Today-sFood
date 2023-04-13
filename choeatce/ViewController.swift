b283796c18ff7ff06545798ce1f40be7e96bf11f
SANGDOLEE <xkdxh625@naver.com>
2023-04-13 16:11:12 +0900

    앱 시작 메인 UI 변경 완료

--- choeatce/ViewController.swift
+++ choeatce/ViewController.swift
@@ -20,14 +20,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var KaKaoMapsButton: UIButton!
     
     @IBOutlet weak var foodName: UILabel! // 음식 Text
-    
+    @IBOutlet weak var mainImage: UIImageView! // 메인 사진
     @IBOutlet weak var imageView: UIImageView! // 음식 변경 버튼
+    @IBOutlet weak var startImage: UIImageView!
+    @IBOutlet weak var startTextLabel: UILabel!
     
     
     var num = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainImage.isHidden = true
+        NaverMapsButton.isHidden = true
+        KaKaoMapsButton.isHidden = true
+        imageView.isHidden = true
         
         // UI, 색상 지정
         self.view.backgroundColor = .systemOrange
@@ -41,6 +48,10 @@ class ViewController: UIViewController {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(changeMenuButton))
         imageView.addGestureRecognizer(tapGesture)
         imageView.isUserInteractionEnabled = true
+        
+        let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(startMenu))
+        startImage.addGestureRecognizer(tapGesture2)
+        startImage.isUserInteractionEnabled = true
         
     }
 
@@ -62,10 +73,24 @@ class ViewController: UIViewController {
     // 카카오 지도로 검색
     @IBAction func goKakao(_ sender: Any) {
         
-        
     }
     
 
+    // 앱 시작 -> 메인 imageView 터치
+    @objc func startMenu() -> Int {
+        
+        mainImage.isHidden = false
+        NaverMapsButton.isHidden = false
+        KaKaoMapsButton.isHidden = false
+        imageView.isHidden = false
+        startImage.isHidden = true
+        startTextLabel.isHidden = true
+        
+        num = Int.random(in: 1...Food.FoodList.count-1)
+        foodName.text = Food.FoodList[num]
+        return num
+    }
+    
     // 메뉴 변경
     @objc func changeMenuButton() -> Int {
         
